c01dcd9734c6a9067816969dd9e063253f8477bd
yanhaixun <yanhaixun@360ser.com>
2017-03-13 14:03:16 +0800

    ?useSSL=false

--- src/main/java/cn/no7player/service/UserService.java
+++ src/main/java/cn/no7player/service/UserService.java
@@ -1,9 +1,11 @@
 package cn.no7player.service;
 
-import cn.no7player.mapper.UserMapper;
-import cn.no7player.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import cn.no7player.mapper.UserMapper;
+import cn.no7player.model.User;
 
 /**
  * Created by zl on 2015/8/27.
@@ -14,7 +16,7 @@ public class UserService {
 
     @Autowired
     private UserMapper userMapper;
-
+    @Transactional
     public User getUserInfo(){
         User user=userMapper.findUserInfo();
         //User user=null;
